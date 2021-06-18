ade9e0f2e470a0372d405bbffc53d79bf7c3fd98
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-18 23:09:24 +0530

    add text-area and button

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#txt-input")
 
-btnTranslate.addEventListener('click', () => console.log("I was clicked"))
+btnTranslate.addEventListener('click', () => {
+    console.log(textInput.value)
+console.log("I was clicked")})
