a5f021a4948fb14c45d1ad574bc84f8e9f7e160a
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-18 23:49:25 +0530

    done input output processing

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate")
-var textInput = document.querySelector("#txt-input")
+var textInput = document.querySelector("[autofocus]")
+var outputBox = document.querySelector(".output-div")
 
 btnTranslate.addEventListener('click', () => {
+
     console.log(textInput.value)
-console.log("I was clicked")})
+    console.log("I was clicked")
+    outputBox.innerHTML = "this is the output" + textInput.value
+})
